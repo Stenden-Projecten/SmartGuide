b2d5a84b64dc4b8ec4c26930ae350f3171c07ca8
Goz3rr <wisse.aarts@zonnet.nl>
2015-05-19 11:58:25 +0200

    Een knop die dingen doet

--- app/src/main/java/com/stenden/smartguide/MainActivity.java
+++ app/src/main/java/com/stenden/smartguide/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "hallo", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
