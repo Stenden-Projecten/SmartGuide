0a5ef06a062cb33cb5bebd0d7f7ed21381618fd0
absol505 <mmte@live.nl>
2015-05-22 13:51:00 +0200

    Kiest nu zelf de database table
    invoer voorbeeld: "leraar, 2" of "lokaal 4"

--- website/db_config.php
+++ website/db_config.php
@@ -6,6 +6,6 @@
  
 define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "innovate"); // database name
+define('DB_DATABASE', "projectinnovate"); // database name
 define('DB_SERVER', "localhost"); // db server
 ?>
--- website/get_product_details.php
+++ website/get_product_details.php
@@ -30,11 +30,30 @@ require_once __DIR__ . '/db_connect.php';
 // connecting to db
 $db = new DB_CONNECT();
  
+$validInput = 0;
+
 // check for post data
 if (isset($_GET["submit"])) {
-    $ID = $_GET["ID"];
+    
+    $validInput = 2;
+    $fullStr = $_GET["ID"];
 
-    $result = mysql_query('SELECT * FROM leraar WHERE ID = '. $ID .' ');
+    $arrayParts = explode(", ", $fullStr);
+    
+    print_r($arrayParts);
+    echo "<br> <br>";
+    
+    if(count($arrayParts)> 1){
+    $table = array_values($arrayParts)[0];
+
+    $idNumber = array_values($arrayParts)[1];
+    
+     if($table == "leraar"){
+    
+         $validInput = 1;
+         
+    // get a leraar from leraar table
+    $result = mysql_query('SELECT * FROM leraar WHERE ID = '. $idNumber .' ');
  
     if (!empty($result)) {
         
@@ -42,18 +61,25 @@ if (isset($_GET["submit"])) {
         if (mysql_num_rows($result) > 0) {
  
             $result = mysql_fetch_array($result);
+ 
             $leraar = array();
             $leraar["ID"] = $result["ID"];
             $leraar["Naam"] = $result["Naam"];
+//            $leraar["price"] = $result["price"];
+//            $leraar["description"] = $result["description"];
+//            $leraar["created_at"] = $result["created_at"];
+//            $leraar["updated_at"] = $result["updated_at"];
+//            // success
             $response["success"] = 1;
+ 
+            // user node
             $response["leraar"] = array();
  
             array_push($response["leraar"], $leraar);
  
+            // echoing JSON response
             echo json_encode($response);
-        } 
-        else 
-        {
+        } else {
             // no leraar found
             $response["success"] = 0;
             $response["message"] = "No leraar found";
@@ -61,29 +87,108 @@ if (isset($_GET["submit"])) {
             // echo no users JSON
             echo json_encode($response);
         }
-    } 
-    else 
-    {
+    } else {
         // no leraar found
     
         $response["success"] = 0;
         $response["message"] = "No leraar found2";
-
+        
+ 
         // echo no users JSON
         echo json_encode($response);
     }
-} 
-else 
-{
-    $response["success"] = 0;
-    $response["message"] = "Required field(s) is missing";
+     }
+     
+          if($table == "lokaal"){
+              
+              $validInput = 1;
+    
+    // get a lokaal from lokaal table
+    $result = mysql_query('SELECT * FROM lokaal WHERE ID = '. $idNumber .' ');
  
+    if (!empty($result)) {
+        
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+ 
+            $result = mysql_fetch_array($result);
+ 
+            $lokaal = array();
+            $lokaal["ID"] = $result["ID"];
+            $lokaal["Nummer"] = $result["Nummer"];
+            $lokaal["Functie"] = $result["Functie"];
+//            $lokaal["description"] = $result["description"];
+//            $lokaal["created_at"] = $result["created_at"];
+//            $lokaal["updated_at"] = $result["updated_at"];
+//            // success
+            $response["success"] = 1;
+ 
+            // user node
+            $response["lokaal"] = array();
+ 
+            array_push($response["lokaal"], $lokaal);
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no lokaal found
+            $response["success"] = 0;
+            $response["message"] = "No lokaal found";
+ 
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no lokaal found
+    
+        $response["success"] = 0;
+        $response["message"] = "No lokaal found2";
+        
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    } }
+    elseif($validInput != 1) {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing or wrong input";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+    }
+    elseif ($validInput == 2) {
+        // submit failed
+    $response["success"] = 0;
+    $response["message"] = "Submit failed! try again.";
+ 
+    // echoing JSON response
     echo json_encode($response);
 }
+
+
+
+}
 ?>
+
+
+
+
+
+
+
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
-            <p>Add Name and E-mail</p>
-            <p>ID: <input type=number name="ID"
+            <p>Add Tabel & ID separated by comma</p>
+            <p>QR: <input type="text" name="ID"
                                  /></p>
                
             <p><input type="submit" name="submit"
