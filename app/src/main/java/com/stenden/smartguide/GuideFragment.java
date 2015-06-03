23263a3ebd109c0747082240725186772f9e8509
zephox <alexiakab@hotmail.com>
2015-06-03 10:53:25 +0200

    kleine edits

--- app/src/main/java/com/stenden/smartguide/GuideFragment.java
+++ app/src/main/java/com/stenden/smartguide/GuideFragment.java
@@ -1,10 +1,6 @@
 package com.stenden.smartguide;
 
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
